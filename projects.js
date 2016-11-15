59ae8ca23e2c2ec66ce726b45dc2f424e00cf19c
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-14 22:42:26 -0600

    added projects to server file

---
+++ projects.js
@@ -0,0 +1,54 @@
+var projects = {
+  projects: [
+    {
+      id: 1,
+      title: 'RISD Tour',
+      skills: 'React &#x25cf; Google Maps &#x25cf; JS &#x25cf; SASS',
+      video: 'RISDTour1.mp4',
+      links: ['https://github.com/doerschera/RISD-react', 'https://risd-tour.herokuapp.com/'],
+      description: "This app is a web-based tour for prospective students who are unable to visit RISD in person. It provides a tour of campus with information about the different departments and spaces within each building. Students can also get insight into RISD student life and academics with links back to the main site for more details."
+    },
+    {
+      id: 2,
+      title: 'Word.ly',
+      skills: 'Express &#x25cf; Handlebars &#x25cf; MySQL &#x25cf; Node',
+      video: 'Wordly.mp4',
+      links: ['https://github.com/doerschera/wordly', 'https://word-ly.herokuapp.com/'],
+      description: "Word.ly is a curated word list for writers. The words can serve as inspiration point or as resource for expanding vocabulary. Users can view and add words, as well as save favorite words via local storage. A MySQL database provides persistent storage and a custom ORM is used to make queries."
+    },
+    {
+      id: 3,
+      title: 'Plum Radio',
+      skills: 'React &#x25cf; JS &#x25cf; SASS',
+      video: 'Plum-Radio.mp4',
+      links: ['https://github.com/doerschera/plum-radio-react', 'https://plum-radio.herokuapp.com/'],
+      description: "Plum Radio is a tool for writers that provides a platform to jumpstart the creative juices. Writers respond to fragments of poems, which act as open ended prompts. In the timed version, Gut Mode, writers have sixty seconds to write as much as they can before the prompt changes, with either five or ten minute sessions. Zen Mode is untimed and allows writers to move at their own pace."
+    },
+    {
+      id: 4,
+      title: 'You Grow, Girl!',
+      skills: 'jQuery &#x25cf; HTML/CSS',
+      video: 'Grow-Girl.mp4',
+      links: ['https://github.com/doerschera/grow-girl', 'https://yougrowgirl.herokuapp.com/'],
+      description: "You Grow Girl! is a flower-themed game designed for young girls in order to promote strength and confidence with basic math skills. Players solve problems by finding the correct order of addition, subtraction, multiplication, and division that makes the two sides of an equation equal. With each correct answer the player's flower grows and a weed withers."
+    },
+    {
+      id: 5,
+      title: 'Dim Valley',
+      skills: 'vanilla JS &#x25cf; jQuery &#x25cf; HTML/CSS',
+      video: 'Dim-Valley.mp4',
+      links: ['https://github.com/doerschera/Dim-Valley', 'https://dim-valley.herokuapp.com/'],
+      description: "Dim Valley is a series of games oriented around saving the town of Dim Vallery from impending darkness. Currently, there are two levels of play: a hangman style game and a game based off the board game Mastermind. With each level with player gets closer to solving a secret ritual that will save the town."
+    },
+    {
+      id: 6,
+      title: 'Objectified',
+      skills: 'Express &#x25cf; jQuery &#x25cf; Charts.js &#x25cf; SASS',
+      video: 'Objectified.mp4',
+      links: ['https://github.com/doerschera/objectified', 'https://object-ified.herokuapp.com/'],
+      description: "Objectified is a short quiz that matches your current mood to an inanimate object. This app is a riff on traditional dating sites, created by correlating some aspect of human emotion to a characteristic of objects. To calculate the result, the difference between each of the user's ratings are compared to the overall difference between each object's rating."
+    }
+  ]
+}
+
+module.exports = projects;
