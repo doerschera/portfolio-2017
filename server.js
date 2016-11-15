7c7da2c28ecd9576063a3fd30a2e30e3193415cf
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-14 18:38:12 -0600

    basic file setup

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var handlebars = require('express-handlebars');
+
+// express handlebars settings
+var app = express();
+app.use(express.static(process.cwd() + '/public'));
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+var projects = require('./projects/projects.js')
+
+// routes
+app.get('/', function(req, res) {
+  res.render('index');
+})
+
+app.get('/work', function(req, res) {
+  res.render('portfolio');
+})
+
+app.get('/about', function(req, res) {
+  res.render('about');
+})
+
+
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, function() {
+  console.log('app listening on port: '+PORT);
+})
